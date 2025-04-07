608c800cde552d075f173c277b4df6e41597d8a4
Boity <mrsboitymbhele@gmail.com>
2025-04-07 18:43:06 +0200

    improving readability and adhering to modern JavaScript best practices

--- javascript/index.js
+++ javascript/index.js
@@ -1,34 +1,32 @@
 function updateTime() {
-  let johannesburgTimeElement = document.querySelector("#Johannesburg .time");
+  const johannesburgTimeElement = document.querySelector("#Johannesburg .time");
   johannesburgTimeElement.innerHTML = moment().format(
     "hh:mm:ss [<small>]a[</small>]"
   );
 
-  let AmsterdamTimeElement = document.querySelector("#Amsterdam .time");
-  AmsterdamTimeElement.innerHTML = moment()
+  const amsterdamTimeElement = document.querySelector("#Amsterdam .time");
+  amsterdamTimeElement.innerHTML = moment()
     .tz("Europe/Amsterdam")
     .format("hh:mm:ss [<small>]a[</small>]");
 }
+
 setInterval(updateTime, 1000);
 updateTime();
 
 function updateCityTime(event) {
-  let cityTimezone = event.target.value;
-  let citiesBodyElement = document.querySelector("#cities");
-  let selectedCity = event.target.options[event.target.selectedIndex].text;
-
-  let cityTime = moment()
-    .tz(cityTimezone)
-    .format("hh:mm:ss [<small>]a[</small>]");
-  let cityMoment = moment().tz(cityTimezone);
+  const cityTimezone = event.target.value;
+  const citiesBodyElement = document.querySelector("#cities");
+  const selectedCity = event.target.options[event.target.selectedIndex].text;
+  const cityMoment = moment().tz(cityTimezone);
+  const cityTime = cityMoment.format("hh:mm:ss [<small>]a[</small>]");
 
   citiesBodyElement.innerHTML = `
-        <div class="city">
-          <h2>${selectedCity}</h2>
-          <div class="date">${cityMoment.format("MMMM Do YYYY")}</div>
-          <div class="time">${cityTime}</div>
-        </div>`;
+    <div class="city">
+      <h2>${selectedCity}</h2>
+      <div class="date">${cityMoment.format("MMMM Do YYYY")}</div>
+      <div class="time">${cityTime}</div>
+    </div>`;
 }
 
-let citiesSelectElement = document.querySelector("#selectedcity");
+const citiesSelectElement = document.querySelector("#selectedcity");
 citiesSelectElement.addEventListener("change", updateCityTime);
