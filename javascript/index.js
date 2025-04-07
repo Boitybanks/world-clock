13e64b39b5dd5a6ad3db3a5cc5c7e9ebabaa927a
Boity <mrsboitymbhele@gmail.com>
2025-04-07 16:39:02 +0200

    added javascript to the select and eventListener

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,35 @@
 function updateTime() {
-  let johannesburgElement = document.getElementById("Johannesburg");
-  let JohannesburgDateElement = document.querySelector("#Johannesburg .date");
-  JohannesburgDateElement.innerHTML = moment().format("MMMM Do YYYY");
-
   let johannesburgTimeElement = document.querySelector("#Johannesburg .time");
   johannesburgTimeElement.innerHTML = moment().format(
     "hh:mm:ss [<small>]a[</small>]"
   );
 
-  //Amsterdam
-  let AmsterdamElement = document.getElementById("Amsterdam");
-  let AmsterdamDateElement = document.querySelector("#Amsterdam .date");
-  JohannesburgDateElement.innerHTML = moment().format("MMMM Do YYYY");
-
   let AmsterdamTimeElement = document.querySelector("#Amsterdam .time");
   AmsterdamTimeElement.innerHTML = moment()
     .tz("Europe/Amsterdam")
     .format("hh:mm:ss [<small>]a[</small>]");
-
-  setInterval(updateTime, 1000);
 }
+setInterval(updateTime, 1000);
 updateTime();
+
+function updateCityTime(event) {
+  let cityTimezone = event.target.value;
+  let citiesBodyElement = document.querySelector("#cities");
+  let selectedCity = event.target.options[event.target.selectedIndex].text;
+  //
+
+  let cityTime = moment()
+    .tz(cityTimezone)
+    .format("hh:mm:ss [<small>]a[</small>]");
+  let cityMoment = moment().tz(cityTimezone);
+
+  citiesBodyElement.innerHTML = `
+        <div class="city">
+          <h2>${selectedCity}</h2>
+          <div class="date">${cityMoment.format("MMMM Do YYYY")}</div>
+          <div class="time">${cityTime}</div>
+        </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#selectedcity");
+citiesSelectElement.addEventListener("change", updateCityTime);
